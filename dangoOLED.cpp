9d4b061099d690589045599de562a07771350d83
jyujyun <93719371+jyujyun@users.noreply.github.com>
2023-11-23 18:56:58 +0900

    Add files via upload

--- dangoOLED.cpp
+++ dangoOLED.cpp
@@ -8,6 +8,9 @@ dangoOLEDライブラリ
 mgo-tec電子工作 https://www.mgo-tec.com/
 SSD1306xledライブラリ https://bitbucket.org/tinusaur/ssd1306xled
 Adafruit OLED ライブラリ https://github.com/adafruit/Adafruit_SSD1306
+
+使用した素材:
+ひらがなフォント https://littlelimit.net/misaki.htm
 ありがとうございます。 Thank you!
 */
 
@@ -16,7 +19,7 @@ Adafruit OLED ライブラリ https://github.com/adafruit/Adafruit_SSD1306
 
 #include "dangoOLED.h"
 #include "font.h";
-
+#include "font_jp.h";
 dangoOLED::dangoOLED(void){}
 
 void dangoOLED::OLED_init(void)
@@ -90,6 +93,22 @@ void dangoOLED::OLED_line(char x1, char y1, char x2, char y2)
      if (e2 < dy) { err += dx;y1 += sy; }
   }
 }
+void dangoOLED::OLED_drawbyte(int dx, char dy, int dw, char dh, const char bmp[])
+{
+	dx *= 8;
+	dw *= 8;
+	int k = 0;
+	for (int j = dy; j < (dh + dy ); j++)
+	{
+		for (int i = dx; i < (dw + dx); i++)
+		{
+			OLED_buff[(j * 128) + i] = pgm_read_byte(&bmp[k]);
+			k+=1;
+		}
+	}
+
+
+}
 void dangoOLED::OLED_box(char x1, char y1, char x2, char y2)
 {
 	for (char i = x1; i <= x2; i++)
@@ -102,6 +121,60 @@ void dangoOLED::OLED_box(char x1, char y1, char x2, char y2)
 		OLED.OLED_pixel(x1, i, 1);
 		OLED.OLED_pixel(x2, i, 1);
 	}
+}
+void dangoOLED::OLED_fillbox(int x1, char y1, int x2, char y2,char color)
+{
+	//color
+	//0 black
+	//1 white
+	//2 gray
+	if (color == 0)
+	{
+		for (int i = x1;i <= x2;i++)
+		{
+			for (char j = y1;j <= y2;j++)
+			{
+				if (i == x1 || i == x2 || j == y1 || j == y2)
+				{
+					OLED.OLED_pixel(i, j, 1);
+				}
+				else
+				{
+					OLED.OLED_pixel(i, j, 0);
+				}
+
+			}
+		}
+	}
+	else if (color == 1)
+	{
+		for (int i = x1;i <= x2;i++)
+		{
+			for (char j = y1;j <= y2;j++)
+			{
+
+				OLED.OLED_pixel(i, j, 1);
+			}
+		}
+	}
+	else if (color == 2)
+	{
+		for (int i = x1;i <= x2;i++)
+		{
+			for (char j = y1;j <= y2;j++)
+			{
+				if ((i % 2 == 0 && j % 2 == 1) || (i % 2 == 1 && j % 2 == 0))
+				{
+					OLED.OLED_pixel(i, j, 1);
+				}
+				else
+				{
+					OLED.OLED_pixel(i, j, 0);
+				}
+			}
+		}
+	}
+
 }
 void dangoOLED::OLED_send(char oled)
 {
@@ -139,25 +212,79 @@ void dangoOLED::OLED_send(char oled)
 		}
 	}
 }
-void dangoOLED::OLED_char(char dx, char dy,char ch,char color = 0) {
-	ch -= 32;
+void dangoOLED::OLED_char(char dx, char dy,uint16_t ch,char color = 0) {
+	
 	dx *= 8;
-	for (char i = 0; i < 6; i++)
+	if (ch < 300)
 	{
-		if (color == 0)
+		ch -= 32;
+		for (char i = 0; i < 6; i++)
 		{
-			OLED_buff[(dy * 128) + dx + i] = pgm_read_byte(&font8[ch * 6 + i]);
+			if (color == 0)
+			{
+				OLED_buff[(dy * 128) + dx + i] = pgm_read_byte(&font8[ch * 6 + i]);
+			}
+			else
+			{
+				OLED_buff[(dy * 128) + dx + i] = ~(pgm_read_byte(&font8[ch * 6 + i]));
+			}
+			OLED_buff[(dy * 128) + dx + 6] = 0xFF * color;
+			OLED_buff[(dy * 128) + dx + 7] = 0xFF * color;
+		}
+	}
+	else
+	{
+		if (ch <= 33215)
+		{
+			ch -= 33153;
 		}
 		else
 		{
-			OLED_buff[(dy * 128) + dx + i] = ~(pgm_read_byte(&font8[ch * 6 + i]));
+			ch -= (33153 + 192);
 		}
-		OLED_buff[(dy * 128) + dx + 6] = 0xFF * color;
-		OLED_buff[(dy * 128) + dx + 7] = 0xFF * color;
-	}
 
+		for (char i = 0; i < 8; i++)
+		{
+			
+			if (color == 0)
+			{
+				OLED_buff[(dy * 128) + dx + i] = pgm_read_byte(&font_jp_8[ch * 8 + i]);
+			}
+			else
+			{
+				OLED_buff[(dy * 128) + dx + i] = ~(pgm_read_byte(&font_jp_8[ch * 8 + i]));
+			}
+
+		}		
+	}
 }
-void dangoOLED::OLED_string(char dx, char dy,char* str,char color = 0) {
+void dangoOLED::OLED_num(char dx,char dy,int num,char color = 0)
+{
+	char pp = 0;
+	if (((num / 10000) % 10) > 0)
+	{
+		pp = 1;
+		OLED_char(dx++,dy,((num / 10000) % 10) + 48);
+	}
+	if (((num / 1000) % 10) > 0 || pp == 1)
+	{
+		pp = 1;
+		OLED_char(dx++,dy,((num / 1000) % 10) + 48);
+	}
+	if (((num / 100) % 10) > 0 || pp == 1)
+	{
+		pp = 1;
+		OLED_char(dx++,dy,((num / 100) % 10) + 48);
+	}
+	if (((num / 10) % 10) > 0 || pp == 1)
+	{
+		pp = 1;
+		OLED_char(dx++,dy,((num / 10) % 10) + 48);
+	}
+	OLED_char(dx++,dy,((num / 1) % 10) + 48);
+}
+void dangoOLED::OLED_string(char dx, char dy,char* str,char color = 0) 
+{
 	while (*str) {
 		OLED_char(dx++,dy,*str++,color);
 	}
--- dangoOLED.h
+++ dangoOLED.h
@@ -8,13 +8,17 @@ dangoOLEDライブラリ
 mgo-tec電子工作 https://www.mgo-tec.com/
 SSD1306xledライブラリ https://bitbucket.org/tinusaur/ssd1306xled
 Adafruit OLED ライブラリ https://github.com/adafruit/Adafruit_SSD1306
+
+使用した素材:
+ひらがなフォント https://littlelimit.net/misaki.htm
 ありがとうございます。 Thank you!
+
 */
 
 
 #include <Arduino.h>
 
-#include <util/delay.h>
+
 
 #ifndef DANGOOLED_H
 #define DANGOOLED_H
@@ -33,9 +37,13 @@ class dangoOLED
 		void OLED_send(char oled);
 		void OLED_pixel(char dx, char dy, char po);
 		void OLED_line(char x1, char y1, char x2, char y2);
-		void OLED_char(char dx, char dy, char ch, char color = 0);
-		void OLED_string(char dx, char dy, char* str, char color = 0);
+		void OLED_char(char dx, char dy, uint16_t ch, char color = 0);
+		void OLED_num(char dx,char dy,int num,char color = 0);
+		void OLED_string(char dx, char dy,char* str,char color = 0);
 		void OLED_box(char x1, char y1, char x2, char y2);
+		void OLED_fillbox(int x1, char y1, int x2, char y2,char color);
+		void OLED_drawbyte(int dx, char dy, int dw, char dh, const char bmp[]);
+		//void tess(void);
 		void syoki(void);
 };
 extern dangoOLED OLED;
